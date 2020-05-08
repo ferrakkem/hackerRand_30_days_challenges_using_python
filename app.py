0eb438c01189d932f0932813f2e1191424bc11bd
ferrakkem <ferrakkem@gmail.com>
2020-05-08 18:32:10 -0400

    Commit of a string, , of length that is indexed from to , print its even-indexed and odd-indexed characters as space-separated strings on a single line

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ In this challenge, we're going to use loops to help us do some simple math. Chec
 Given an integer,
 print its first multiples. Each multiple (where ) should be printed on a new line in the form: n x i = result.
 '''
+from pip._vendor.distlib.compat import raw_input
+
 
 def multiples_each(n):
 
@@ -27,4 +29,27 @@ def runner_up():
     arr_list = list(arr_set)
     arr_list.sort(reverse=True)
     print(arr_list[1])
-runner_up()
+#runner_up()
+
+'''
+Task
+Given a string, , of length that is indexed from to , print its even-indexed and odd-indexed characters as
+space-separated strings on a single line (see the Sample below for more detail).
+
+'''
+
+def string_sperate():
+    #reomve_Enter your string number:
+    num = int(input('Enter your string number: '))
+    for i in range(1, num+1):
+        user_string = input('Enter your string')
+        even_char = []
+        odd_char = []
+        for i in range(len(user_string)):
+            if i%2 == 0:
+                even_char.append(user_string[i])
+            else:
+                odd_char.append(user_string[i])
+        print(''.join(even_char), ''.join(odd_char))
+
+string_sperate()
